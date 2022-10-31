33ff612900f6c7aee79316b9a0a6c7fa39cb8ff6
NGHT-Tur <arthurgmota2020@gmail.com>
2022-10-31 14:49:34 -0300

    Aula 71

---
+++ config.js
@@ -0,0 +1,16 @@
+
+import firebase from "firebase/app";
+require("@firebase/firestore")
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFbdBXalEXmHp-iX1tIGlNj8lRGlphJ9k",
+  authDomain: "tedio-d26d1.firebaseapp.com",
+  projectId: "tedio-d26d1",
+  storageBucket: "tedio-d26d1.appspot.com",
+  messagingSenderId: "707836751489",
+  appId: "1:707836751489:web:9e7468abeb03c9f680e780"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
